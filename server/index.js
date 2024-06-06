3790f10596892ffa97906e7b979c0f1d5ea4d70f
Prabhat Kumar <pkr.876550@gmail.com>
2024-06-06 21:16:24 +0530

    configured redis, prisma and postgresql services

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import http from 'http'
 import socketServices from './services/socket.js'
+import dotenv from 'dotenv'
+dotenv.config({ path: './.env' });
 
 const socketService = new socketServices()
 
@@ -8,7 +10,7 @@ const httpServer = http.createServer()
 socketService.io.attach(httpServer)
 socketService.initListners()
 
-const PORT = 8000
+const PORT = process.env.PORT ||8000
 httpServer.listen(PORT, () => {
         console.log(`Http Server running at ${PORT}`)
 })
--- server/services/socket.js
+++ server/services/socket.js
@@ -1,4 +1,6 @@
 import {Server} from "socket.io"
+import prismaClient from "./prisma.js"
+import {pub, sub} from './redis.js'
 
 class socketServices{
      #io
@@ -10,18 +12,34 @@ class socketServices{
                 origin : "*"
             }
         })
+        sub.subscribe('MESSAGES')
     }
 
-    initListners(){
+    initListners (){
 
         const io = this.#io
-        console.log('init socket listnres ....')
+        console.log('init socket listners ....')
         io.on('connect', (socket) => {
             console.log(`New socket connected ${socket.id}`)
 
-            socket.on('event:message' ,({ message }) =>{
+            socket.on('event:message' ,async ( message ) =>{
                 console.log('New message Rec.', message)
+                //publish this message to redis
+                await pub.publish("MESSAGES", JSON.stringify(message) )
             })
+        })
+
+        sub.on("message", async (channel, message) => {
+            if(channel === "MESSAGES" ){
+                console.log("New message from Redis", message)
+                this.#io.emit("message", message)
+                //store message in db
+                await prismaClient.message.create({
+                    data:{
+                        text : message
+                    }
+                })
+            }
         })
     }
 
